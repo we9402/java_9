7a9a72f65d631dc9aed6803b5dc94b5c23dcc65c
we9402 <kblm157@naver.com>
2018-02-22 11:13:12 +0900

    tsert

--- src/com/jung/s1/Member.java
+++ src/com/jung/s1/Member.java
@@ -2,13 +2,87 @@ package com.jung.s1;
 
 public class Member {
 	//이름, 나이, 성별, 몸무게, 키, 전화번호
-	String name; //이름
-	int age;	//나이
-	String s;
-	double weight;
-	double height;
-	String number;
+	private String name; //이름
+	private int age;	//나이
+	private String s;
+	private double weight;
+	private double height;
+	private String number;
 	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public String getS() {
+		return s;
+	}
+
+
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+
+
+	public double getWeight() {
+		return weight;
+	}
+
+
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+
+
+	public double getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+
+
+	public String getNumber() {
+		return number;
+	}
+
+
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+
+
 	public void info() {
 		System.out.println("이름 : "+name);
 		System.out.println("나이 : "+age);
---
+++ src/com/jung/s2/MainTest_1.java
@@ -0,0 +1,12 @@
+package com.jung.s2;
+
+public class MainTest_1 {
+
+	public static void main(String[] args) {
+		Point p = new Point();
+		p.info();
+		p.setNum(23);
+		p.setName("iu");
+	}
+
+}
